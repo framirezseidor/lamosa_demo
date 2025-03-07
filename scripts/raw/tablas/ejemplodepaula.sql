dd8d4e381450d053a4775b70924509b0a22c2f4e
framirezseidor <framirez@seidoranalytics.com>
2025-03-07 13:48:18 -0600

    subir cambios

--- scripts/raw/tablas/ejemplodepaula.sql
+++ scripts/raw/tablas/ejemplodepaula.sql
@@ -1,6 +1,8 @@
-CREATE or alter TABLE pedidos.public.ejemplopaula (
+CREATE or alter TABLE LAMOSALAKE_PRD.RAW."SQ1_0MATERIAL_ATTR" (
     nombre varchar(100),
     apellido varchar(100),
+
     direccion varchar(100),
-    cambio number
+    cambio number,
+        NEWCOL,
     );
