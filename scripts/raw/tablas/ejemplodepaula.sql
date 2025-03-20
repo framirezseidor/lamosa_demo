f0c2dd1f47522d1b0f73bca69c7222374d8a57a7
framirezseidor <framirez@seidoranalytics.com>
2025-03-19 18:50:50 -0600

    schemachange

--- scripts/raw/tablas/ejemplodepaula.sql
+++
@@ -1,8 +0,0 @@
-CREATE or alter TABLE LAMOSALAKE_PRD.RAW."SQ1_0MATERIAL_ATTR" (
-    nombre varchar(100),
-    apellido varchar(100),
-
-    direccion varchar(100),
-    cambio number,
-        NEWCOL,
-    );
